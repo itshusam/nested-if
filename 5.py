a087cfd7df8cd5a86b368263ab00c6318748b38a
itshusam <161774514+itshusam@users.noreply.github.com>
2024-03-07 16:50:47 -0600

    Update 5.py

--- 5.py
+++ 5.py
@@ -5,3 +5,9 @@ if cpu_usage > 90:
     print("High CPU usage!")
 elif cpu_usage > 80 and cpu_usage <= 90 :
     pass
+
+torage = random.randint(0, 1000)
+if storage > 900:
+    print("you are running out of storage!!")
+elif storage > 700 and storage <900 :
+    pass
